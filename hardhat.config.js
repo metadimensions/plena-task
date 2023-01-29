345c4fe9d86a7d78de0dd9e0c51051ee5a664310
Venkimanian <venkimanian1995@gmail.com>
2023-01-29 21:38:59 +0530

    updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+// hardhat.config.js
+const { infuraProjectId, mnemonic, etherscanApiKey } = require('./secrets.json');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-etherscan");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.17",
+
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraProjectId}`,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ""
+  },
+  solidity: "0.8.17"
 };
